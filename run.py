c41562eac0bac1551eafb81c8d80a26358c790ae
Bulle1982 <bullites114@hotmail.com>
2023-12-12 01:12:39 +0000

    Treasure Island Game

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to Treasure Island!")
+print("Your mission is to find the treasure.")
+
+direction = input("Would you take left or right?\n").lower()
+if direction == "left":
+    action = input("Would you swim or wait? Swim or Wait?\n").lower()
+    if action == "wait":
+        door = input("Which door would you choose? Red, Yellow or Blue?\n").lower()
+        if door == "red":
+            print("You are burnt by fire. Game over!")
+        elif door == "blue":
+            print("You are eaten by beasts. Game over!")
+        elif door == "yellow":
+            print("You won!")
+        else:
+            print("You chose a door that does not exist. Game over!")            
+    else:
+        print("You are attacked by a trout. Game over!")
+else:
+    print("You fell into a hole. Game over!")
+
+                    
